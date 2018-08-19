4fe04ec540ffeee56d794ab02a157fd6c2a05a68
James Dempsey <dempsey@tapas-software.net>
2018-08-19 12:13:57 -0700

    Update to allow Xcode 9.3-compatible project files. - Resolves #16

--- BuildSettingExtractor/BuildSettingExtractor.m
+++ BuildSettingExtractor/BuildSettingExtractor.m
@@ -14,7 +14,7 @@ static NSSet *XcodeCompatibilityVersionStringSet() {
     static NSSet *_compatibilityVersionStringSet;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        _compatibilityVersionStringSet = [NSSet setWithObjects:@"Xcode 3.2", @"Xcode 6.3", @"Xcode 8.0", nil];
+        _compatibilityVersionStringSet = [NSSet setWithObjects:@"Xcode 3.2", @"Xcode 6.3", @"Xcode 8.0", @"Xcode 9.3", nil];
     });
     return _compatibilityVersionStringSet;
 }
