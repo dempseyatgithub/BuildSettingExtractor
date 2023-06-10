3c541f8110544dd12920738575b8ae49f815eed2
James Dempsey <dempsey@tapas-software.net>
2023-06-10 07:30:21 -0700

    Add script and make adjustments for Xcode Cloud - Add post clone script to write private xcconfig files - Update paths to private xcconfig files

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+#  ci_post_clone.sh
+#  CloudBuildTestbed
+#
+#  Created by James Dempsey on 6/10/23.
+#
+
+if [ $CI = "TRUE" ]; then
+
+# Write private project configuration file
+privateProjectConfigPath="$CI_WORKSPACE/Config/PrivateProjectConfig.xcconfig"
+
+cat > $privateProjectConfigPath <<- EOF
+DEVELOPMENT_TEAM = $TS_DEVELOPMENT_TEAM
+EOF
+
+# Write private app configuration file
+privateAppConfigPath="$CI_WORKSPACE/Config/PrivateAppConfig.xcconfig"
+
+cat > $privateAppConfigPath <<- EOF
+PRODUCT_BUNDLE_IDENTIFIER = $TS_PRODUCT_BUNDLE_IDENTIFIER
+EOF
+
+else
+echo "CI env variable was not TRUE"
+fi
