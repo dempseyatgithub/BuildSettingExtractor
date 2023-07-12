ea4ae30f52d8f0f522e3cd088e9c3bd967fc5ea9
supeng.charlie <supeng.charlie@bytedance.com>
2023-07-12 15:23:48 +0800

    support cli

---
+++ BuildSettingExtractorCli/Briding-Header.h
@@ -0,0 +1,14 @@
+//
+//  Header.h
+//  BuildSettingExtractorCli
+//
+//  Created by ByteDance on 2023/7/11.
+//  Copyright © 2023 Tapas Software. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#import "BuildSettingExtractor.h"
+
+#endif /* Header_h */
