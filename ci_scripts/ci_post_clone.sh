ead2d4fbe27f192a50d61d4a1bc93b51a522e614
James Dempsey <dempsey@tapas-software.net>
2023-12-09 09:21:54 -0800

    Use built-in Xcode Cloud env variables
    Use the built-in Xcode Cloud environment variables
    CI_TEAM_ID and CI_BUNDLE_ID in the post-clone ci script.
    
    Since these values are private and not in the public repository
    they are added in conditional xcconfig files written before
    building.
    
    The workflow had them added as custom environment variables but
    the values are also already present as Xcode Cloud environment
    variables.

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -12,14 +12,14 @@ if [ $CI_XCODE_CLOUD = "TRUE" ]; then
 privateProjectConfigPath="$CI_PRIMARY_REPOSITORY_PATH/Config/PrivateProjectConfig.xcconfig"
 
 cat > $privateProjectConfigPath <<- EOF
-DEVELOPMENT_TEAM = $TS_DEVELOPMENT_TEAM
+DEVELOPMENT_TEAM = $CI_TEAM_ID
 EOF
 
 # Write private app configuration file
 privateAppConfigPath="$CI_PRIMARY_REPOSITORY_PATH/Config/PrivateAppConfig.xcconfig"
 
 cat > $privateAppConfigPath <<- EOF
-PRODUCT_BUNDLE_IDENTIFIER = $TS_PRODUCT_BUNDLE_IDENTIFIER
+PRODUCT_BUNDLE_IDENTIFIER = $CI_BUNDLE_ID
 EOF
 
 else
