b62e427a63ff861eebcc37dffff50bce9027de70
James Dempsey <dempsey@tapas-software.net>
2024-03-02 07:40:03 -0800

    Update script for use with Xcode Cloud.

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,22 +1,25 @@
 #!/bin/sh
 
 #  ci_post_clone.sh
-#  CloudBuildTestbed
+#  BuildSettingExtractor
 #
 #  Created by James Dempsey on 6/10/23.
 #
 
 if [ $CI_XCODE_CLOUD = "TRUE" ]; then
 
+# Get path to xcconfig files in project
+configFolderPath = "$CI_PRIMARY_REPOSITORY_PATH/Config"
+
 # Write private project configuration file
-privateProjectConfigPath="$CI_PRIMARY_REPOSITORY_PATH/Config/PrivateProjectConfig.xcconfig"
+privateProjectConfigPath="$configFolderPath/PrivateProjectConfig.xcconfig"
 
 cat > $privateProjectConfigPath <<- EOF
 DEVELOPMENT_TEAM = $CI_TEAM_ID
 EOF
 
 # Write private app configuration file
-privateAppConfigPath="$CI_PRIMARY_REPOSITORY_PATH/Config/PrivateAppConfig.xcconfig"
+privateAppConfigPath="$configFolderPath/PrivateAppConfig.xcconfig"
 
 cat > $privateAppConfigPath <<- EOF
 PRODUCT_BUNDLE_IDENTIFIER = $CI_BUNDLE_ID
